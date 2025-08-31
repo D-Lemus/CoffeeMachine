b28106879cdfbde2062dbb50ac3678795436ff9f
D-Lemus <diego.lemus@iteso.mx>
2025-08-30 20:11:06 -0600

    snake_case

--- coffeeMachine.py
+++ coffeeMachine.py
@@ -1,6 +1,6 @@
 
 
-class CoffeeMachine:
+class coffee_machine:
     #Capacity constants
     MAX_BEANS = 200
     MAX_CUPS = 10
@@ -26,64 +26,64 @@ class CoffeeMachine:
 
     def __init__(self):
         self.beans = 200
-        self.cups = 10
+        self.cups  = 10
         self.water = 2500
-        self.milk = 1000
+        self.milk  = 1000
 
-    def showData(self):
+    def show_data(self):
         print(f"COFEE MACHINE HAS:\nBeans: {self.beans} \nCups: {self.cups} \nWater: {self.water} \nMilk: {self.milk}\n=============================\n")
 
 
-    def makeCofee(self,coffeeType,count):
+    def make_coffee(self,coffeeType,count):
         #Depending the cofffee type the ifs statement checks if removing the amount needed for each coffee is affordable, if it is then we calculate the difference 
         for i in range(count):
             #ESPRESSO
-            if coffeeType ==1:
-                if ((self.beans - CoffeeMachine.ESPRESSO_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - CoffeeMachine.ESPRESSO_WATER) >= 0):
+            if coffeeType == 1:
+                if ((self.beans - coffee_machine.ESPRESSO_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - coffee_machine.ESPRESSO_WATER) >= 0):
 
-                    self.beans -= CoffeeMachine.ESPRESSO_BEANS
+                    self.beans -= coffee_machine.ESPRESSO_BEANS
                     self.cups -= 1
-                    self.water -= CoffeeMachine.ESPRESSO_WATER
+                    self.water -= coffee_machine.ESPRESSO_WATER
                     print(f"Espresso #{i+1} Ready\n=============================\n")
 
                 else: print(f"Espresso #{i+1} could not be filled because 1 or more ingredients is missing\n=============================\n")
 
             #LATTE
-            elif coffeeType ==2:
-                if ((self.beans - CoffeeMachine.LATTE_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - CoffeeMachine.LATTE_WATER) >= 0) and (self.milk - CoffeeMachine.LATTE_MILK) >= 0:
+            elif coffeeType == 2:
+                if ((self.beans - coffee_machine.LATTE_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - coffee_machine.LATTE_WATER) >= 0) and (self.milk - coffee_machine.LATTE_MILK) >= 0:
 
-                    self.beans -= CoffeeMachine.LATTE_BEANS
-                    self.milk -= CoffeeMachine.LATTE_MILK
+                    self.beans -= coffee_machine.LATTE_BEANS
+                    self.milk -= coffee_machine.LATTE_MILK
                     self.cups -= 1
-                    self.water -= CoffeeMachine.LATTE_WATER
+                    self.water -= coffee_machine.LATTE_WATER
                     print(f"Latte  #{i+1} Ready\n=============================\n")
 
                 else: print(f"Latte #{i+1} could not be filled because 1 or more ingredients is missing\n=============================\n")
             #CAPUCCINO
-            elif coffeeType ==3:
-                if ((self.beans - CoffeeMachine.CAPUCCINO_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - CoffeeMachine.CAPUCCINO_WATER) >= 0) and (self.milk - CoffeeMachine.CAPUCCINO_MILK) >= 0:
+            elif coffeeType == 3:
+                if ((self.beans - coffee_machine.CAPUCCINO_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - coffee_machine.CAPUCCINO_WATER) >= 0) and (self.milk - coffee_machine.CAPUCCINO_MILK) >= 0:
 
-                    self.beans -= CoffeeMachine.CAPUCCINO_BEANS
-                    self.milk -= CoffeeMachine.CAPUCCINO_MILK
+                    self.beans -= coffee_machine.CAPUCCINO_BEANS
+                    self.milk -= coffee_machine.CAPUCCINO_MILK
                     self.cups -= 1
-                    self.water -= CoffeeMachine.CAPUCCINO_WATER
+                    self.water -= coffee_machine.CAPUCCINO_WATER
                 
                     print(f"Capuccino  #{i+1} Ready\n=============================\n")
 
                 else: print(f"Capuccino #{i+1} could not be filled because 1 or more ingredients is missing\n=============================\n")
 
 
-    def machineFill(self):
+    def machine_fill(self):
         #Checks if theres stuff to refill and then calculates the amount needed to fill AND avoid it from overflowing
-        if (self.beans < CoffeeMachine.MAX_BEANS or self.cups < CoffeeMachine.MAX_CUPS or self.water < CoffeeMachine.MAX_WATER or self.milk < CoffeeMachine.MAX_MILK):
+        if (self.beans < coffee_machine.MAX_BEANS or self.cups < coffee_machine.MAX_CUPS or self.water < coffee_machine.MAX_WATER or self.milk < coffee_machine.MAX_MILK):
             
             #Lambda to OverKill
             calculate_refill = lambda currentStorage, maxStorage: maxStorage - currentStorage
 
-            beansToAdd= calculate_refill(self.beans,CoffeeMachine.MAX_BEANS)
-            cupsToAdd = calculate_refill(self.cups,CoffeeMachine.MAX_CUPS)
-            waterToAdd = calculate_refill(self.water,CoffeeMachine.MAX_WATER)
-            milkToAdd = calculate_refill(self.milk,CoffeeMachine.MAX_MILK)
+            beansToAdd= calculate_refill(self.beans,coffee_machine.MAX_BEANS)
+            cupsToAdd = calculate_refill(self.cups,coffee_machine.MAX_CUPS)
+            waterToAdd = calculate_refill(self.water,coffee_machine.MAX_WATER)
+            milkToAdd = calculate_refill(self.milk,coffee_machine.MAX_MILK)
 
             self.beans += beansToAdd 
             self.cups += cupsToAdd
@@ -93,7 +93,7 @@ class CoffeeMachine:
             print(f"Added Beans: {beansToAdd}\nAdded Cups: {cupsToAdd}\nAdded Water: {waterToAdd}\nAdded Milk: {milkToAdd}\n=============================\n")
         else: print("\n=============================\nMachine Already Filled\n=============================\n")
 
-def paymentMethod(self):  
+def payment_method(self):  
     total = 0 
     while True:
         try:
@@ -127,14 +127,14 @@ while True:
 
 #MAIN
 leave = False
-c = CoffeeMachine()
+c = coffee_machine()
 print("Welcome To JavaCofeeStore.")
 
 print()
 
 while not leave: 
 
-    print(f"What would YOU like?\n\n\t1 - Make ESPRESSO:  {CoffeeMachine.ESPRESSO_PRICE}$\n\t2 - Make LATTE:     {CoffeeMachine.LATTE_PRICE}$\n\t3 - Make CAPUCCINO: {CoffeeMachine.CAPUCCINO_PRICE}$")
+    print(f"What would YOU like?\n\n\t1 - Make ESPRESSO:  {coffee_machine.ESPRESSO_PRICE}$\n\t2 - Make LATTE:     {coffee_machine.LATTE_PRICE}$\n\t3 - Make CAPUCCINO: {coffee_machine.CAPUCCINO_PRICE}$")
     print(f"\t4 - ADMINISTRATE:\n\t5 - LEAVE")
     opcion =int(input("Choose option: ").strip())
 
@@ -142,17 +142,17 @@ while not leave:
     try:
         if opcion ==1: 
             latteNumber = int(input("How Many?: ").strip())
-            c.makeCofee(1,latteNumber)
+            c.make_coffee(1,latteNumber)
             input("Press ENTER to continue...")
 
         elif opcion ==2: 
             espressoNumber = int(input("How Many?: ").strip())
-            c.makeCofee(2,espressoNumber)
+            c.make_coffee(2,espressoNumber)
             input("Press ENTER to continue...")
 
         elif opcion ==3:
             capuccinoNumber = int(input("How Many?: ").strip()) 
-            c.makeCofee(3,capuccinoNumber)
+            c.make_coffee(3,capuccinoNumber)
             input("Press ENTER to continue...")
 
         elif opcion ==4: 
@@ -167,10 +167,10 @@ while not leave:
                         
 
                         if adminOption == 1:
-                            c.showData()
+                            c.show_data()
                             input("Press ENTER to continue...")
                         elif adminOption == 2:
-                            c.machineFill()
+                            c.machine_fill()
                             input("Press ENTER to continue...")
                         elif adminOption == 3:
                             break
@@ -180,6 +180,7 @@ while not leave:
             else:
                 print("Wrong Password. Bye.\n")
                 input("Press ENTER to exit...\n")
+                
                 
         elif opcion ==5: 
             leave = True
