c9a7512dc3d54c19a2ba39ca27697dcc0ab7dcd4
D-Lemus <diego.lemus@iteso.mx>
2025-08-31 09:33:33 -0600

    des-espaguetificacion de la maquina de cafe separandola en funciones junto con la integracion de los tamanos

--- coffeeMachine.py
+++ coffeeMachine.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-class CoffeeType(Enum):
+class coffee_type(Enum):
     ESPRESSO_SMALL= 1
     ESPRESSO_MEDIUM = 2
     ESPRESSO_LARGE = 3
@@ -11,143 +11,190 @@ class CoffeeType(Enum):
     CAPUCCINO_MEDIUM = 8
     CAPUCCINO_LARGE = 9
 
+
 COFFEE_INGREDIENTS = {
     # ESPRESSO
-    CoffeeType.ESPRESSO_SMALL:  {"water": 30, "beans": 15, "milk": 0, "cups_small": 1,  "price": 4},
-    CoffeeType.ESPRESSO_MEDIUM: {"water": 50, "beans": 16, "milk": 0, "cups_small": 1, "price": 5},
-    CoffeeType.ESPRESSO_LARGE:  {"water": 70, "beans": 18, "milk": 0, "cups_small": 1,  "price": 6},
+    coffee_type.ESPRESSO_SMALL:  {"water": 30, "beans": 15, "milk": 0, "cup_size": "small",  "price": 4},
+    coffee_type.ESPRESSO_MEDIUM: {"water": 50, "beans": 16, "milk": 0, "cup_size": "small", "price": 5},
+    coffee_type.ESPRESSO_LARGE:  {"water": 70, "beans": 18, "milk": 0, "cup_size": "small",  "price": 6},
 
     # LATTE
-    CoffeeType.LATTE_SMALL:  {"water": 250, "beans": 18, "milk": 50,  "cups_small": 1,  "price": 6},
-    CoffeeType.LATTE_MEDIUM: {"water": 300, "beans": 20, "milk": 75,  "cups_medium": 1, "price": 7},
-    CoffeeType.LATTE_LARGE:  {"water": 350, "beans": 22, "milk": 100, "cups_large": 1,  "price": 8},
+    coffee_type.LATTE_SMALL:  {"water": 30, "beans": 8,  "milk": 220, "cup_size": "small", "price": 6}, 
+    coffee_type.LATTE_MEDIUM: {"water": 30, "beans": 8,  "milk": 270, "cup_size": "medium", "price": 7},
+    coffee_type.LATTE_LARGE:  {"water": 30, "beans": 8,  "milk": 320, "cup_size": "large", "price": 8},
 
-    # CAPUCCINO
-    CoffeeType.CAPUCCINO_SMALL:  {"water": 250, "beans": 12, "milk": 80, "cups_small": 1,  "price": 5},
-    CoffeeType.CAPUCCINO_MEDIUM: {"water": 300, "beans": 14, "milk": 100, "cups_medium": 1, "price": 6},
-    CoffeeType.CAPUCCINO_LARGE:  {"water": 350, "beans": 16, "milk": 120, "cups_large": 1,  "price": 7}
+    # CAPUCCINO 
+    coffee_type.CAPUCCINO_SMALL:  {"water": 30, "beans": 8,  "milk": 180, "cup_size": "small", "price": 6},
+    coffee_type.CAPUCCINO_MEDIUM: {"water": 30, "beans": 8,  "milk": 240, "cup_size": "medium", "price": 7},
+    coffee_type.CAPUCCINO_LARGE:  {"water": 30, "beans": 8,  "milk": 310, "cup_size": "large", "price": 8} 
 }
 
 
 class coffee_machine:
     #Capacity constants
     MAX_BEANS = 200
+    MAX_CUPS_SMALL = 10
+    MAX_CUPS_MEDIUM = 10
+    MAX_CUPS_LARGE = 10
     MAX_CUPS = 10
     MAX_WATER = 2500
     MAX_MILK = 1000
 
-    #Ingredient Cost constants for each Cofee Type
-    ESPRESSO_WATER,ESPRESSO_BEANS = 250,16
-    LATTE_WATER,LATTE_MILK, LATTE_BEANS = 250,75,20
-    CAPUCCINO_WATER,CAPUCCINO_MILK, CAPUCCINO_BEANS = 200,100,12
-
     #Cofee Finance
-    coffeeBank = 0
-    espressoSold = 0
-    latteSold = 0
-    capuccinoSold = 0
+    coffee_bank = 0
+    espresso_sold = 0
+    latte_sold = 0
+    capuccino_sold = 0
 
 
     def __init__(self):
         self.beans = 200
-        self.cups  = 10
+        self.cups_small  = 10
+        self.cups_medium  = 10
+        self.cups_large  = 10
         self.water = 2500
         self.milk  = 1000
 
     def show_data(self):
         print(f"COFEE MACHINE HAS:\nBeans: {self.beans} \nCups: {self.cups} \nWater: {self.water} \nMilk: {self.milk}\n=============================\n")
+    
+    def get_cups_inventory(self,cup_size):
+        #Returns inventory based on the size
+        cup_inventory = {
+            "small" : self.cups_small,
+            "medium" : self.cups_medium,
+            "large" : self.cups_large
+
+        }
+        return cup_inventory.get(cup_size)
+
+    def _consume_cup(self, cup_size):
+        """Consumes cup in relation to the inventory"""
+
+        if cup_size == "small":
+            self.cups_small -= 1
+        if cup_size == "medium":
+            self.cups_small -= 1
+        if cup_size == "large":
+            self.cups_small -= 1
 
 
-    def make_coffee(self,coffeeType,count):
-        #Depending the cofffee type the ifs statement checks if removing the amount needed for each coffee is affordable, if it is then we calculate the difference 
-        for i in range(count):
-            #ESPRESSO
-            if coffeeType == 1:
-                if ((self.beans - coffee_machine.ESPRESSO_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - coffee_machine.ESPRESSO_WATER) >= 0):
+    def _has_enough_ingredients(self, coffee_type):
+        """Checks for each ingredient in the machine to be more that the amount needed"""
+        return (self.beans >= coffee_type['beans'] and
+            self.cups>= 1 and
+            self.water >=coffee_type['water'] and
+            self.milk >= coffee_type['milk'])
 
-                    self.beans -= coffee_machine.ESPRESSO_BEANS
-                    self.cups -= 1
-                    self.water -= coffee_machine.ESPRESSO_WATER
-                    print(f"Espresso #{i+1} Ready\n=============================\n")
+    def _consume_ingredients(self, coffee_type):
+        coffee_type = COFFEE_INGREDIENTS[coffee_type]
+        cup_size = coffee_type["cup_size"]
 
-                else: print(f"Espresso #{i+1} could not be filled because 1 or more ingredients is missing\n=============================\n")
+        self.beans -= coffee_type['beans']
+        self.water -= coffee_type['water']
+        self.beans -= coffee_type['milk']
+        self._consume_cup(cup_size)
 
-            #LATTE
-            elif coffeeType == 2:
-                if ((self.beans - coffee_machine.LATTE_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - coffee_machine.LATTE_WATER) >= 0) and (self.milk - coffee_machine.LATTE_MILK) >= 0:
+    def _make_one_coffee(self,coffee_type,coffee_number):
+        """Depending the cofffee type the ifs statement checks if removing the amount needed for each coffee is affordable, if it is then we calculate the difference """
+        
+        if self ._has_enough_ingredients(coffee_type):
+            self._consume_ingredients(coffee_type)
+            recipe = COFFEE_INGREDIENTS[coffee_type]
+            self.coffee_bank += recipe["price"]
+            print(f"{coffee_type.name} #{coffee_number} is ready")
+            return True
+        else:       
+            print(f"{coffee_type.name} #{coffee_number} could not be made due to 1 or more ingredients missing")
+            return False
 
-                    self.beans -= coffee_machine.LATTE_BEANS
-                    self.milk -= coffee_machine.LATTE_MILK
-                    self.cups -= 1
-                    self.water -= coffee_machine.LATTE_WATER
-                    print(f"Latte  #{i+1} Ready\n=============================\n")
 
-                else: print(f"Latte #{i+1} could not be filled because 1 or more ingredients is missing\n=============================\n")
-            #CAPUCCINO
-            elif coffeeType == 3:
-                if ((self.beans - coffee_machine.CAPUCCINO_BEANS) >= 0 and (self.cups - 1) >= 0 and (self.water - coffee_machine.CAPUCCINO_WATER) >= 0) and (self.milk - coffee_machine.CAPUCCINO_MILK) >= 0:
+    def make_coffees(self,coffee_type,how_many):
+        """Makes multiple coffees with a limit of 3 coffees per machine"""
+        success =0
+        if how_many <= 3:
+            for i in range(how_many):
+                if self._make_one_coffee(coffee_type,i+1) == True:
+                    success += 1
+                else:
+                    break
 
-                    self.beans -= coffee_machine.CAPUCCINO_BEANS
-                    self.milk -= coffee_machine.CAPUCCINO_MILK
-                    self.cups -= 1
-                    self.water -= coffee_machine.CAPUCCINO_WATER
-                
-                    print(f"Capuccino  #{i+1} Ready\n=============================\n")
+            if coffee_type.startswith("ESPRESSO"):
+                self.espresso_sold += success
+            elif coffee_type.startswith("LATTE"):
+                self.latte_sold += success
+            elif coffee_type.startswith("CAPUCCINO"):
+                self.capuccino_sold += success
 
-                else: print(f"Capuccino #{i+1} could not be filled because 1 or more ingredients is missing\n=============================\n")
+        return success
 
 
     def machine_fill(self):
         #Checks if theres stuff to refill and then calculates the amount needed to fill AND avoid it from overflowing
-        if (self.beans < coffee_machine.MAX_BEANS or self.cups < coffee_machine.MAX_CUPS or self.water < coffee_machine.MAX_WATER or self.milk < coffee_machine.MAX_MILK):
+        if (self.beans < coffee_machine.MAX_BEANS or self.cups < coffee_machine.MAX_CUPS_SMALL or self.water < coffee_machine.MAX_WATER or self.milk < coffee_machine.MAX_MILK or self.cups < coffee_machine.MAX_CUPS_MEDIUM or self.cups < coffee_machine.MAX_CUPS_LARGE):
             
-            #Lambda to OverKill
-            calculate_refill = lambda currentStorage, maxStorage: maxStorage - currentStorage
+            def calculate_refill(currentStorage, maxStorage):
+                return maxStorage - currentStorage
 
-            beansToAdd= calculate_refill(self.beans,coffee_machine.MAX_BEANS)
-            cupsToAdd = calculate_refill(self.cups,coffee_machine.MAX_CUPS)
-            waterToAdd = calculate_refill(self.water,coffee_machine.MAX_WATER)
-            milkToAdd = calculate_refill(self.milk,coffee_machine.MAX_MILK)
+            beans_to_add= calculate_refill(self.beans,coffee_machine.MAX_BEANS)
+            cups_to_add_small = calculate_refill(self.cups,coffee_machine.MAX_CUPS_SMALL)
+            cups_to_add_medium = calculate_refill(self.cups,coffee_machine.MAX_CUPS_MEDIUM)
+            cups_to_add_large = calculate_refill(self.cups,coffee_machine.MAX_CUPS_LARGE)
+            water_to_add = calculate_refill(self.water,coffee_machine.MAX_WATER)
+            milk_to_add = calculate_refill(self.milk,coffee_machine.MAX_MILK)
 
-            self.beans += beansToAdd 
-            self.cups += cupsToAdd
-            self.water += waterToAdd
-            self.milk += milkToAdd
+            self.beans += beans_to_add 
+            self.cups_small += cups_to_add_small
+            self.cups_medium += cups_to_add_medium
+            self.cups_large += cups_to_add_large
+            self.water += water_to_add
+            self.milk += milk_to_add
 
-            print(f"Added Beans: {beansToAdd}\nAdded Cups: {cupsToAdd}\nAdded Water: {waterToAdd}\nAdded Milk: {milkToAdd}\n=============================\n")
-        else: print("\n=============================\nMachine Already Filled\n=============================\n")
+            print(f"Added Beans: {beans_to_add}\n")
+            print(f"Added Small Cups: {cups_to_add_small}\n")
+            print(f"Added Medium Cups: {cups_to_add_medium}\n")
+            print(f"Added Large Cups: {cups_to_add_large}\n")
+            print(f"Added Water: {water_to_add}\n")
+            print(f"Added Milk: {milk_to_add}\n")
+            print("=" * 12)
 
-def payment_method(self):  
-    total = 0 
+        else: 
+            print("=" * 12)
+            print("Machine Already Filled.")
+            print("=" * 12)
+
+        
+    def payment_method(self):  
+        total = 0 
+        while True:
+            try:
+                donate = int(input("You want to donate a dollar to dog shelter \n\n\t1 - YES\n\t2 - NO, IN OTHER MOMENT\n> ").strip())
+                if (donate == 1):
+                    total += 1
+                    print(f"Thanks, your new total is ${total}")
+                    break
+                elif (donate == 2):
+                    print(f"Thanks, your total is ${total}")
+                    break
+                else:
+                    print("Enter a valid number")
+            except ValueError:
+                print("Enter a valid number")
+
     while True:
         try:
-            donate = int(input("You want to donate a dollar to dog shelter \n\n\t1 - YES\n\t2 - NO, IN OTHER MOMENT\n> ").strip())
-            if (donate == 1):
-                total += 1
-                print(f"Thanks, your new total is ${total}")
+            total = 0 
+            method =int(input("What is yor payment method?\n\n\t1 - CARD\n\t2 - CASH").strip())
+            if (method == 1):
+                print(f"Thanks, you are to going pay ${total} with card")
                 break
-            elif (donate == 2):
-                print(f"Thanks, your total is ${total}")
+            elif (method == 2):
+                print(f"Thanks, you are to going pay ${total} with cash")
                 break
             else:
                 print("Enter a valid number")
         except ValueError:
-            print("Enter a valid number")
-
-while True:
-    try:
-        total = 0 
-        method =int(input("What is yor payment method?\n\n\t1 - CARD\n\t2 - CASH").strip())
-        if (method == 1):
-            print(f"Thanks, you are to going pay ${total} with card")
-            break
-        elif (method == 2):
-            print(f"Thanks, you are to going pay ${total} with cash")
-            break
-        else:
-            print("Enter a valid number")
-    except ValueError:
-        print("Enter a valid number")       
+            print("Enter a valid number")       
 
 #MAIN
 leave = False
@@ -157,7 +204,7 @@ print("Welcome To JavaCofeeStore.")
 print()
 
 while not leave: 
-
+    
     print(f"What would YOU like?\n\n\t1 - Make ESPRESSO:  {coffee_machine.ESPRESSO_PRICE}$\n\t2 - Make LATTE:     {coffee_machine.LATTE_PRICE}$\n\t3 - Make CAPUCCINO: {coffee_machine.CAPUCCINO_PRICE}$")
     print(f"\t4 - ADMINISTRATE:\n\t5 - LEAVE")
     opcion =int(input("Choose option: ").strip())
@@ -205,7 +252,7 @@ while not leave:
                 print("Wrong Password. Bye.\n")
                 input("Press ENTER to exit...\n")
                 
-                
+
         elif opcion ==5: 
             leave = True
 
